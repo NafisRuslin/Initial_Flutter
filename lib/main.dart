e078423cd4b2c2a2789c398df8f190e9fba2bbfa
NR <nafisruslin@gmail.com>
2026-04-16 03:19:21 +0800

    pikachu

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.yellow,
-          body: Center(child: Image.network('https://www.denofgeek.com/wp-content/uploads/2021/04/Pikachu.png?resize=768%2C432')),
-          appBar: AppBar(backgroundColor: Colors.blueGrey[900],
-            title: Center(child: Text("TIDO")
-            ),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                height: 150,
+                margin: EdgeInsets.symmetric(
+                  vertical: 50,
+                  horizontal: 10,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Muhammad Nafis bin Ruslin",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'monospace',
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmGPnnDlAYc8FqeIWGnkiYrYXuqUUHx_V3Ww&s',
+                      height: 60,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.lightBlueAccent,
+                width: double.infinity,
+                height: 150,
+                margin: EdgeInsets.symmetric(
+                  vertical: 50,
+                  horizontal: 10,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Nafis",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'serif',
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSabcn1BiasMaBw96S3bVHQJ-zh7X4qJmgdGg&s',
+                      height: 60,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.yellow,
+                width: double.infinity,
+                height: 150,
+                margin: EdgeInsets.symmetric(
+                  vertical: 50,
+                  horizontal: 10,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Ruslin",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Times New Roman',
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Image.network(
+                      'https://readrange.com/wp-content/uploads/2023/10/RR-hero-1-768x480.jpeg',
+                      height: 60,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        )
+        ),
+
       ),
-  );
+    );
+  }
 }
